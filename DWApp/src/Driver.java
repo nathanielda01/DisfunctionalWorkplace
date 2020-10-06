06746ebf1f9f2812114aa91e80f4005cab79011e
cprestera <44652309+cprestera@users.noreply.github.com>
2020-10-06 16:01:42 -0600

    Created print menu in driver code.

--- DWApp/src/Driver.java
+++ DWApp/src/Driver.java
@@ -1,17 +1,40 @@
 import Company.Organization;
 
+import java.util.Scanner;
+
 
 // TODO
 public class Driver {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
         Organization organization = new Organization();
         organization.printWelcome();
         organization.printOrganization();
+        appMenu();
+    }
 
+    public static void appMenu() {
+        String input = "";
+        Scanner scanner = new Scanner(System.in);
 
-
+        try {
+            while (!input.equals("Quit")) {
+                System.out.println("***Company Actions***");
+                System.out.println("To hire someone type: \"Hire: New Name\"");
+                System.out.println("To fire someone type: \"Fire: Full Name\"");
+                System.out.println("Let someone quit type: \"Quit: Full Name\"");
+                System.out.println("Lay off someone type: \"Layoff: Full Name\"");
+                System.out.println("To transfer someone type: \"Transfer: Full Name\"");
+                System.out.println("To promote someone type: \"Promote: Full Name\"");
+                System.out.println("To print current organization type: \"Print\"");
+                System.out.println("To quit the application type: \"Quit\"");
+                System.out.print("Input: ");
+                input = (input.replaceAll(input, scanner.nextLine().strip()));
+                System.out.println();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }
     }
 }
